85006e9ffb80ce2ff18e09b5b53086e6fef4a998
lswatson16 <lauralynw16@gmail.com>
2022-09-21 11:43:27 -0400

    Add steps and 2nd solution

--- mumbling.js
+++ mumbling.js
@@ -2,7 +2,18 @@
 Mumbling is a 7kyu coding problem found on Codewars
 Instructions can be found here: https://www.codewars.com/kata/5667e8f4e3f572a8f2000039
 
-I solved this problem using JavaScript
+Examples:
+accum("abcd") -> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt") -> "C-Ww-Aaa-Tttt"
+
+
+I solved this problem using JavaScript.
+
+Steps:
+- convert the string to an array
+- map over the array and return an array of concatenated strings
+- convert the array to a string separated by dashes
 */
 
 function accum(s) {
@@ -12,5 +23,13 @@ function accum(s) {
       let str = el.toUpperCase() + el.repeat(i).toLowerCase();
       return str;
     })
+    .join("-");
+}
+
+// 2nd attempt
+function accum(s) {
+  return s
+    .split("")
+    .map((el, i) => el.toUpperCase() + el.toLowerCase().repeat(i))
     .join("-");
 }

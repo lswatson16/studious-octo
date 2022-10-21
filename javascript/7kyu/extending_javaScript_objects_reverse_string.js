56ad0d22a25f9e9fde93d284dcdae650127ab9dd
lswatson16 <lauralynw16@gmail.com>
2022-10-20 23:04:23 -0400

    Add code wars challenge and solution extending String object

---
+++ javascript/7kyu/extending_javaScript_objects_reverse_string.js
@@ -0,0 +1,16 @@
+/*
+Extending JavaScript Objects: Reverse String is a 7kyu coding problem found on Codewars
+Instructions can be found here: https://www.codewars.com/kata/581270cb4927602fc800005a
+
+Unfortunately, there's no a .reverse() method for the JavaScript String object.
+
+Your task is to extend JavaScript String object, so you can reverse strings just like this:
+'Hello, World!'.reverse();
+
+The method shoulf return:
+'!dlroW ,olleH'
+*/
+
+String.prototype.reverse = function () {
+  return this.split("").reverse().join("");
+};

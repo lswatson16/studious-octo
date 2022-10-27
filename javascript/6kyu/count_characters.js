35f59c0ca088700ef2213ee53bd653838e6f0636
lswatson16 <lauralynw16@gmail.com>
2022-10-27 12:11:27 -0400

    Add solution and steps to solve counting characters in string

---
+++ javascript/6kyu/count_characters.js
@@ -0,0 +1,25 @@
+/*
+"Count characters in your string" is a 6kyu coding problem found on Codewars
+Instructions can be found here: https://www.codewars.com/kata/52efefcbcdf57161d4000091
+*/
+
+/* 
+||| Solution A |||
+
+Steps:
+- convert the input string to an array
+- iterate through the array to create an object that keeps count of each character
+- if the key doesn't exist in the obj then create the key and assign a value 1
+- if the key exists then increment the value by 1 (increase the count value for the char key)
+*/
+
+function count(string) {
+  return string.split("").reduce((acc, char) => {
+    if (!acc[char]) {
+      acc[char] = 1;
+    } else {
+      acc[char]++;
+    }
+    return acc;
+  }, {});
+}

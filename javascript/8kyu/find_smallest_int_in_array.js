ae2df68caa11340b7b990a140a84c9e063c68043
lswatson16 <lauralynw16@gmail.com>
2022-11-23 00:02:12 -0500

    Add steps to solve kata find the smallests integer in an array

---
+++ javascript/8kyu/find_smallest_int_in_array.js
@@ -0,0 +1,16 @@
+/*
+"Find the smallest integer in the array" is a 8kyu coding problem found on Codewars
+Instructions can be found here: https://www.codewars.com/kata/55a2d7ebe362935a210000b2
+*/
+
+/* Solution A
+
+Steps to solve:
+- sort the integers in ascending order (low to high)
+- return the first element in the array which is the smallest integer
+*/
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    return args.sort((a, b) => a - b)[0];
+  }
+}

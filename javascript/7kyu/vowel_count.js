56f95da25375a55ab715efdc055ff75ff4e6c4ae
lswatson16 <lauralynw16@gmail.com>
2022-11-25 23:49:53 -0500

    Add steps to solve kata

--- javascript/7kyu/vowel_count.js
+++ javascript/7kyu/vowel_count.js
@@ -2,6 +2,11 @@
 "Vowel Count!" is a 7kyu coding problem found on Codewars
 Instructions can be found here: https://www.codewars.com/kata/54ff3102c1bad923760001f3
 
+Steps:
+- keep count of the vowels by initializing a vowel count to 0
+- check each character in the input string...
+  - if the character is a vowel (a, e, i, ,o, u) then increment the vowel count by 1
+- return the vowel count as an output integer
 */
 
 function getCount(str) {

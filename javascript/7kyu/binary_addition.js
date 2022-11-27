35425ddb4845bd068d1747628cd859631f84d640
lswatson16 <lauralynw16@gmail.com>
2022-11-27 17:14:45 -0500

    Add solution to kata, binary addition

---
+++ javascript/7kyu/binary_addition.js
@@ -0,0 +1,11 @@
+/*
+"Binary addition" is a 7kyu coding problem found on Codewars
+Instructions can be found here: https://www.codewars.com/kata/551f37452ff852b7bd000139
+
+Steps:
+- Convert the sum of two input values to a binary number
+*/
+
+function addBinary(a,b){
+  return (a + b).toString(2)
+}

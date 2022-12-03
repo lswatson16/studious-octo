b4e77cecf0ca81521f871ba5e96ffd3d1ae2365a
lswatson16 <lauralynw16@gmail.com>
2022-12-02 20:40:37 -0500

    Solve kata who likes this

---
+++ javascript/6kyu/who_likes_this.js
@@ -0,0 +1,27 @@
+/*
+"Who likes it?" is a 6kyu coding problem found on Codewars
+Instructions can be found here: https://www.codewars.com/kata/5266876b8f4bf2da9b000362
+
+Steps:
+- return an interpolated string based on the length of the names input
+*/
+
+function likes(names) {
+  switch (names.length) {
+    case 0:
+      return 'no one likes this';
+      break;
+    case 1:
+      return `${names[0]} likes this`;
+      break;
+    case 2:
+      return `${names[0]} and ${names[1]} like this`;
+      break;
+    case 3:
+      return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+      break;
+    default:
+      return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+      break;
+  }
+}

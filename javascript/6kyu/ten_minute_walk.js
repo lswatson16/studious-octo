177d681f6f9f96826208e5b72cdfc07ebe67d288
lswatson16 <lauralynw16@gmail.com>
2022-12-30 19:45:24 -0500

    Solve kata: validate 10 minute walk

---
+++ javascript/6kyu/ten_minute_walk.js
@@ -0,0 +1,43 @@
+/*
+"Take a Ten Minutes Walk" is a 6kyu coding problem found on Codewars.
+Instructions can be found here --> https://www.codewars.com/kata/54da539698b8a2ad76000228/
+
+--------------------------------------------------------------
+Description:
+
+You live in the city of Cartesia where all roads are laid out in a perfect grid. You arrived ten minutes too early 
+to an appointment, so you decided to take the opportunity to go for a short walk. The city provides its citizens 
+with a Walk Generating App on their phones -- everytime you press the button it sends you an array of one-letter strings 
+representing directions to walk (eg. ['n', 's', 'w', 'e']). You always walk only a single block for each letter (direction) 
+and you know it takes you one minute to traverse one city block, so create a function that will return true if the walk 
+the app gives you will take you exactly ten minutes (you don't want to be early or late!) and will, of course, 
+return you to your starting point. Return false otherwise.
+
+Note: you will always receive a valid array containing a random assortment of direction letters ('n', 's', 'e', or 'w' only). 
+It will never give you an empty array (that's not a walk, that's standing still!).
+
+--------------------------------------------------------------
+*/
+
+// ---------- Solution A ----------
+function isValidWalk(walk) {
+  let x = 0;
+  let y = 0;
+
+  walk.forEach((w) => {
+    if (w === 'n') {
+      y++;
+    }
+    if (w === 's') {
+      y--;
+    }
+    if (w === 'e') {
+      x++;
+    }
+    if (w === 'w') {
+      x--;
+    }
+  });
+
+  return walk.length === 10 && x === 0 && y === 0 ? true : false;
+}

9f068fa569b4035bf3f18d3c4e6c072c57f9a7d4
lswatson16 <lauralynw16@gmail.com>
2023-01-10 18:18:43 -0500

    Add solution to kata: growth of population

---
+++ javascript/7kyu/growth_of_a_population.js
@@ -0,0 +1,17 @@
+/*
+"Growth of a Population" is a 7 kyu coding problem found on Codewars.
+Instructions can be found here --> https://www.codewars.com/kata/563b662a59afc2b5120000c6/
+
+--------------------------------------------------------------
+*/
+
+// ---------- Solution A ----------
+const nbYear = (p0, percent, aug, p) => {
+  let startPop = p0;
+  let years = 0;
+
+  for (years; startPop < p; years++) {
+    startPop += Math.floor(startPop * (percent / 100) + aug);
+  }
+  return years;
+};

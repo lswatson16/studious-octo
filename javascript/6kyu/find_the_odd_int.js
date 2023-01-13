9bc680b35f0ee502d4af42e6ad82dd055b02ef38
lswatson16 <lauralynw16@gmail.com>
2023-01-12 22:10:16 -0500

    Solve kata: find the odd int

---
+++ javascript/6kyu/find_the_odd_int.js
@@ -0,0 +1,34 @@
+/*
+"Find the odd int" is a 6 kyu coding problem found on Codewars.
+Instructions can be found here --> https://www.codewars.com/kata/54da5a58ea159efa38000836/
+
+--------------------------------------------------------------
+Description:
+
+Given an array of integers, find the one that appears an odd number of times.
+
+There will always be only one integer that appears an odd number of times.
+
+Examples:
+[7] should return 7, because it occurs 1 time (which is odd).
+[0] should return 0, because it occurs 1 time (which is odd).
+[1,1,2] should return 2, because it occurs 1 time (which is odd).
+[0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+[1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+
+--------------------------------------------------------------
+*/
+
+// ---------- Solution A ----------
+const findOdd = (A) => {
+  const count = A.reduce((acc, int) => {
+    !acc[int] ? (acc[int] = 1) : (acc[int] += 1);
+    return acc;
+  }, {});
+
+  for (const k in count) {
+    if (count[k] % 2 !== 0) {
+      return Number(k);
+    }
+  }
+};

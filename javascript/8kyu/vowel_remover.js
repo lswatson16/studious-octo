4f309bd0206c7759101e99ab860935592d17f660
lswatson16 <lauralynw16@gmail.com>
2023-01-18 20:21:58 -0500

    Add multiple solutions to kata: vowel remover

---
+++ javascript/8kyu/vowel_remover.js
@@ -0,0 +1,30 @@
+/*
+"Vowel remover" is a 8 kyu coding problem found on Codewars.
+Instructions can be found here --> https://www.codewars.com/kata/5547929140907378f9000039/train/javascript
+
+--------------------------------------------------------------
+Description:
+
+Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
+
+Examples:
+"hello"     -->  "hll"
+"codewars"  -->  "cdwrs"
+"goodbye"   -->  "gdby"
+"HELLO"     -->  "HELLO"
+don't worry about uppercase vowels
+y is not considered a vowel for this kata
+
+--------------------------------------------------------------
+*/
+
+// ---------- Solution A ----------
+const shortcut = (string) => {
+  return string.replace(/[a,e,i,o,u]/g, '');
+};
+
+// ---------- Solution B ----------
+const shortcut = (string) => {
+  let regex = /[a,e,i,o,u]/g;
+  return string.split(regex).join('');
+};

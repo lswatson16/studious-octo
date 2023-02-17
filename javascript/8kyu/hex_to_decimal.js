5baf113ce8fc8f88c78c282415aa224aa2a94d11
lswatson16 <lauralynw16@gmail.com>
2023-02-17 12:05:56 -0500

    Convert hexadecimal number (string) to a decimal

---
+++ javascript/8kyu/hex_to_decimal.js
@@ -0,0 +1,9 @@
+/*
+"Hex to Decimal" is a 8 kyu coding problem found on Codewars.
+Instructions can be found here --> https://www.codewars.com/kata/57a4d500e298a7952100035d/
+--------------------------------------------------------------
+
+*/
+
+// ---------- Solution A ----------
+const hexToDec = (hexString) => parseInt(hexString, 16);
